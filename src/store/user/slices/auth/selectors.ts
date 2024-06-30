904dded2a533300a247419aa55cdb7aac54a54bd
黑云白土 <Li_Yucong@outlook.com>
2024-06-30 16:04:32 +0800

    🐛 fix: Revert: 7d4f28f391fa8cf52d74be1a39ff3ff3030aeaca

--- src/store/user/slices/auth/selectors.ts
+++ src/store/user/slices/auth/selectors.ts
@@ -48,8 +48,8 @@ const isLogin = (s: UserStore, signOut: () => void) => {
   if (!s.enableAuth()) return true;
 
   if (
-    !publicMetadata ||
-    (publicMetadata.registrationUrl && publicMetadata.registrationUrl.includes('oaknuts.me'))
+    publicMetadata?.registrationUrl &&
+    publicMetadata.registrationUrl.includes('https://oaknuts.me/')
   ) {
     signOut();
     throw new UserNotAllowLogIn();
